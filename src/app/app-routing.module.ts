c1a659a7a93427b1925a234b065b2026d3c7f885
yunlong <laureler@163.com>
2019-02-24 22:55:27 +0800

    添加setup模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SetupComponent} from './pages/setup/setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/setup', pathMatch: 'full'},
+  {path: 'setup', component: SetupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
