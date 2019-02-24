f5fda49bb8fcdd2582eeec41c52397a8a7846ab5
yunlong <laureler@163.com>
2019-02-24 23:31:44 +0800

    添加setup模块

--- src/app/pages/setup/setup.component.ts
+++ src/app/pages/setup/setup.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocalStorageService} from '../../services/local-storage.service';
+
+// 用户名      USERNAME
+// 初始化标识    INIT_FLAG
+// 启动日期     START_USING_DATE
+import {USERNAME, INIT_FLAG, START_USING_DATE} from '../../services/local-storage.namespace';
+import {getTodayTime} from '../../../util/time';
 
 @Component({
-  selector: 'app-setup',
-  templateUrl: './setup.component.html',
-  styleUrls: ['./setup.component.less']
+	selector: 'app-setup',
+	templateUrl: './setup.component.html',
+	styleUrls: ['./setup.component.less']
 })
 export class SetupComponent implements OnInit {
+	username: string;
 
-  constructor() { }
+	// private 参数意味着依赖注入
+	constructor(private store: LocalStorageService) {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	completeSetup(): void {
+		this.store.set(INIT_FLAG, true);
+		this.store.set(START_USING_DATE, getTodayTime());
+		this.store.set(USERNAME, this.username);
+	}
 }
--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SetupComponent } from './setup.component';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {SetupComponent} from './setup.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd'
 
 @NgModule({
-  declarations: [SetupComponent],
-  imports: [
-    CommonModule
-  ]
+	declarations: [SetupComponent],
+	imports: [
+		CommonModule,
+		FormsModule,
+		NgZorroAntdModule
+	]
 })
-export class SetupModule { }
+export class SetupModule {
+}
